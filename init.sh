f538741e04e2d25546560e70cb168e674e6f5784
Thibault Lacroux <thibault.lacroux@gmail.com>
2019-10-13 12:21:11 +0200

    Removing submodule to avoid zplug clone of it at install

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git clone https://github.com/gnachman/iterm2-website
 cd iterm2-website && git fetch && git checkout origin/master
 cd ..
 cp -R iterm2-website/source/shell_integration .
